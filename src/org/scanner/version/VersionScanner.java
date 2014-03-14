02f1a89c0490821e6c016fc9288411eff7733f13
amarkevich <amarkevich@talend.com>
2014-03-14 17:53:12 +0300

    use pax-url-mvn

--- src/org/scanner/version/VersionScanner.java
+++ src/org/scanner/version/VersionScanner.java
@@ -1,137 +1,159 @@
 package org.scanner.version;
 
 import java.io.File;
+import java.io.FileInputStream;
+import java.io.IOException;
+import java.io.InputStream;
+import java.net.URL;
+import java.net.URLStreamHandler;
+import java.net.URLStreamHandlerFactory;
 import java.util.HashMap;
 import java.util.HashSet;
 import java.util.Map;
+import java.util.Properties;
 import java.util.Set;
 
 import javax.xml.parsers.DocumentBuilder;
 import javax.xml.parsers.DocumentBuilderFactory;
 
+import org.ops4j.pax.url.mvn.Handler;
+import org.ops4j.pax.url.mvn.internal.Parser;
 import org.w3c.dom.Document;
 import org.w3c.dom.Element;
 import org.w3c.dom.Node;
 
 public class VersionScanner {
 
-	private static class Artifact {
-		public String group;
-		public String id;
+    private static class Artifact {
+        private final String group;
+        private final String id;
 
-		@Override
-		public boolean equals(Object obj) {
-			if (obj instanceof Artifact) {
-				Artifact artifact = (Artifact) obj;
-				return id.equals(artifact.id) && group.equals(artifact.group);
-			}
-			return super.equals(obj);
-		}
-		@Override
-		public int hashCode() {
-			return id.hashCode() ^ group.hashCode();
-		}
-	}
+        public Artifact(String group, String id) {
+            this.group = group;
+            this.id = id;
+        }
 
-	private static DocumentBuilder DB;
-	private Map<Artifact, Map<String, Set<String>>> artifacts;
+        @Override
+        public boolean equals(Object obj) {
+            if (obj instanceof Artifact) {
+                Artifact artifact = (Artifact) obj;
+                return id.equals(artifact.id) && group.equals(artifact.group);
+            }
+            return super.equals(obj);
+        }
+        @Override
+        public int hashCode() {
+            return id.hashCode() ^ group.hashCode();
+        }
+    }
 
-	public void process(String directory) {
-		artifacts = new HashMap<Artifact, Map<String, Set<String>>>();
-		scanDir(new File(directory), directory.length());
-		for (Map.Entry<Artifact, Map<String, Set<String>>> entryArtifact : artifacts.entrySet()) {
-			Map<String, Set<String>> version = entryArtifact.getValue();
-			if (version.size() > 1) {
-				Artifact artifact = entryArtifact.getKey();
-				System.out.println("group '" + artifact.group + "' artifact '" + artifact.id + "':");
-				for (Map.Entry<String, Set<String>> entryVersion : version.entrySet()) {
-					System.out.println("\tversion '" + entryVersion.getKey() + '\'');
-					for(String name : entryVersion.getValue()) {
-						System.out.println("\t\t@ " + name);
-					}
-				}
-			}
-		}
-	}
+    private static DocumentBuilder DB;
+    private final Map<Artifact, Map<String, Set<String>>> artifacts = new HashMap<Artifact, Map<String, Set<String>>>();
 
-	private void scanDir(File dir, int base) {
-		for (File file : dir.listFiles()) {
-			if (file.isDirectory()) {
-				scanDir(file, base);
-			} else {
-				if (file.getName().endsWith(".xml")) {
-					parseXml(getDocument(file), file.getAbsolutePath().substring(base));
-				}
-			}
-		}
-	}
+    public void process(File directory) throws IOException {
+        Properties properties = new Properties();
+        InputStream is = new FileInputStream(new File(directory, "etc/org.apache.karaf.features.cfg"));
+        properties.load(is);
+        is.close();
 
-	private void parseXml(Document xml, String name) {
-		Element features = xml.getDocumentElement();
-		if(!"features".equals(features.getNodeName())) {
-			return;
-		}
-		for (Node nodeFeature = features.getFirstChild(); nodeFeature != null; nodeFeature = nodeFeature.getNextSibling()) {
-			if (nodeFeature.getNodeType() != Node.ELEMENT_NODE) {
-				continue;
-			}
-			Element feature = (Element)  nodeFeature;
-			for (Node nodeBundle = feature.getFirstChild(); nodeBundle != null; nodeBundle = nodeBundle.getNextSibling()) {
-				if (nodeBundle.getNodeType() != Node.ELEMENT_NODE
-						|| !"bundle".equals(nodeBundle.getNodeName())) {
-					continue;
-				}
-				Element bundle = (Element) nodeBundle;
-				String mvnPath = bundle.getTextContent();
-				mvnPath = mvnPath.substring(mvnPath.indexOf(':') + 1);
+        File system = new File(directory, "system");
+        System.setProperty("org.ops4j.pax.url.mvn.localRepository", system.getPath());
+        System.setProperty("org.ops4j.pax.url.mvn.repositories", system.toURI().toString());
+        URL.setURLStreamHandlerFactory(new URLStreamHandlerFactory() {
+            @Override
+            public URLStreamHandler createURLStreamHandler(String protocol) {
+                if ("mvn".equals(protocol)) {
+                    return new Handler();
+                }
+                return null;
+            }
+        });
 
-				Artifact artifact = new Artifact();
-				int index = mvnPath.indexOf('/');
-				artifact.group = mvnPath.substring(0, index);
-				mvnPath = mvnPath.substring(index + 1);
-				index = mvnPath.indexOf('/');
-				artifact.id = mvnPath.substring(0, index);
-				String version = mvnPath.substring(index + 1);
-				Map<String, Set<String>> versions = artifacts.get(artifact);
-				if (null == versions) {
-					versions = new HashMap<String, Set<String>>();
-					artifacts.put(artifact, versions);
-				}
-				Set<String> names = versions.get(version);
-				if (null == names) {
-					names = new HashSet<String>();
-					versions.put(version, names);
-				}
-				names.add(name);
-			}
-		}
-		
-	}
+        String featuresRepositories = (String) properties.get("featuresRepositories");
+        for(String featuresRepository : featuresRepositories.split(",")) {
+            URL url = new URL(featuresRepository);
+            is = url.openStream();
 
-	private static Document getDocument(File file) {
-		try {
-			if (null == DB) {
-				//get the factory
-				DocumentBuilderFactory dbf = DocumentBuilderFactory.newInstance();
-				dbf.setValidating(false);
-				dbf.setFeature("http://apache.org/xml/features/nonvalidating/load-external-dtd", false);
-	
-				//Using factory get an instance of document builder
-				DB = dbf.newDocumentBuilder();
-			}
-	
-			//parse using builder to get DOM representation of the XML file
-			return DB.parse(file);
-		} catch (Exception e) {
-			throw new RuntimeException(e);
-		}
-	}
+            parseXml(getDocument(is), featuresRepository);
 
-	public static void main(String[] args) {
-		if (args.length < 1) {
-			System.out.println("specify Karaf system directory to scan");
-		}
-		new VersionScanner().process(args[0]);
-	}
+            is.close();
+        }
+
+        for (Map.Entry<Artifact, Map<String, Set<String>>> entryArtifact : artifacts.entrySet()) {
+            Map<String, Set<String>> version = entryArtifact.getValue();
+            if (version.size() > 1) {
+                Artifact artifact = entryArtifact.getKey();
+                System.out.println("[" + artifact.group + "] " + artifact.id);
+                for (Map.Entry<String, Set<String>> entryVersion : version.entrySet()) {
+                    System.out.println('\t' + entryVersion.getKey());
+                    for(String name : entryVersion.getValue()) {
+                        System.out.println("\t\t" + name);
+                    }
+                }
+            }
+        }
+    }
+
+    private void parseXml(Document xml, String name) throws IOException {
+        Element features = xml.getDocumentElement();
+        if(!"features".equals(features.getNodeName())) {
+            return;
+        }
+        for (Node nodeFeature = features.getFirstChild(); nodeFeature != null; nodeFeature = nodeFeature.getNextSibling()) {
+            if (nodeFeature.getNodeType() != Node.ELEMENT_NODE) {
+                continue;
+            }
+            Element feature = (Element)  nodeFeature;
+            for (Node nodeBundle = feature.getFirstChild(); nodeBundle != null; nodeBundle = nodeBundle.getNextSibling()) {
+                if (nodeBundle.getNodeType() != Node.ELEMENT_NODE
+                        || !"bundle".equals(nodeBundle.getNodeName())) {
+                    continue;
+                }
+                Element bundle = (Element) nodeBundle;
+                Parser mvnPath = new Parser(bundle.getTextContent().substring("mvn:".length()));
+
+                Artifact artifact = new Artifact(mvnPath.getGroup(), mvnPath.getArtifact());
+                Map<String, Set<String>> versions = artifacts.get(artifact);
+                if (null == versions) {
+                    versions = new HashMap<String, Set<String>>();
+                    artifacts.put(artifact, versions);
+                }
+                String version = mvnPath.getVersion();
+                Set<String> names = versions.get(version);
+                if (null == names) {
+                    names = new HashSet<String>();
+                    versions.put(version, names);
+                }
+                names.add(name);
+            }
+        }
+        
+    }
+
+    private static Document getDocument(InputStream is) {
+        try {
+            if (null == DB) {
+                //get the factory
+                DocumentBuilderFactory dbf = DocumentBuilderFactory.newInstance();
+                dbf.setValidating(false);
+                dbf.setFeature("http://apache.org/xml/features/nonvalidating/load-external-dtd", false);
+    
+                //Using factory get an instance of document builder
+                DB = dbf.newDocumentBuilder();
+            }
+    
+            //parse using builder to get DOM representation of the XML file
+            return DB.parse(is);
+        } catch (Exception e) {
+            throw new RuntimeException(e);
+        }
+    }
+
+    public static void main(String[] args) throws Exception {
+        if (args.length < 1) {
+            System.out.println("specify Karaf directory to scan");
+        }
+        new VersionScanner().process(new File(args[0]));
+    }
 
 }
